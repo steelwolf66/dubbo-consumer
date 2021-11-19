bb4d0694f569965e49a7437e9a0412a06323e819
SUN <15144639564@163.com>
2021-11-19 16:18:59 +0800

    增加引用BaseController

--- src/main/java/com/wolf/consumer/controller/ConsumerController.java
+++ src/main/java/com/wolf/consumer/controller/ConsumerController.java
@@ -1,20 +1,27 @@
 package com.wolf.consumer.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.wolf.common.entity.BaseController;
 import com.wolf.common.service.ProviderService;
+import com.wolf.common.utils.HttpUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @RequestMapping("/consumer")
-public class ConsumerController {
-    @Reference(lazy = true,check = false)
+public class ConsumerController extends BaseController {
+
+    @Reference(lazy = true)
     private ProviderService providerService;
 
     @GetMapping("/info")
     public String invokeProvider(){
         providerService.testDubbo();
-        return "RPC";
+        logger.info("after provider");
+        HttpUtils.getResponse().setStatus(HttpServletResponse.SC_OK);
+        return "Test Dubbo RPC success";
     }
 }
